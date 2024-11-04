8cfcf96e0c66163502a293f9b40905ed77bb6cf3
muddassirirshad786 <muddassir.irshad786@gmail.com>
2024-11-04 10:34:25 +0500

    Add basic str

---
+++ Application/Features/Product/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Product.Queries
+{
+    public class GetAllProductsQuery : IRequest<IEnumerable<Domain.Entites.Product>>
+    {
+        internal class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<Domain.Entites.Product>>
+        {
+            public async Task<IEnumerable<Domain.Entites.Product>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+            {
+               var list = new List<Domain.Entites.Product>();
+                for (int i = 0; i < 50; i++)
+                {
+                    var pro = new Domain.Entites.Product();
+                    pro.Name = "test name";
+                    pro.Description = "test des";
+                    pro.Rate = 50 + i;
+                    list.Add(pro);
+                }
+                return list;
+            }
+        }
+    }
+}
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Product.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public readonly IMediator _mediatR;
+
+        public ProductsController(IMediator mediatR)
+        {
+            _mediatR = mediatR;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts(CancellationToken cancellationToken)
+        {
+            var result = await _mediatR.Send(new GetAllProductsQuery(),cancellationToken);
+            return Ok(result);
+        }
+    }
+}
